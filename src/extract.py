0b1ceec6e60ff00fcb4e152df6e0a04e0be40c50
Lyndon Fan <lyndonfan@wessexinsights.com>
2023-11-09 12:49:55 +0000

    feat: save raw file before exrtacting

--- src/extract.py
+++ src/extract.py
@@ -1,12 +1,42 @@
+import os
+from datetime import datetime, timezone
+import orjson
 from pathlib import Path
+from typing import Any
+
+from fsspec.implementations.local import LocalFileSystem
+# from gcsfs import GCSFileSystem
 from fhir.resources.R4B.bundle import Bundle, BundleEntry
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Extractor:
-    def __init__(self):
-        pass
+    def __init__(self, filesystem: str = "local"):
+        self.bucket = os.environ["BUCKET"]
+        self.filesystem = filesystem
+        if filesystem == "local":
+            self.fs = LocalFileSystem(True)
+        # elif filesystem == "google":
+        #     self.fs = GCSFileSystem()
+        else:
+            raise ValueError(f"Unsupported filesystem: {filesystem}")
+
+    def save_raw_file(self, data: dict[str, Any], original_path: Path):
+        time_now = datetime.now(timezone.utc)
+        # not use ":" as windows or other file systems may think it's special
+        upload_time_string = time_now.isoformat(timespec="seconds").replace(":", "-")
+        new_base_name = f"{original_path.stem}_{upload_time_string}.json"
+        new_path = Path(self.bucket) / f"upload_date={time_now.strftime('%Y-%m-%d')}" / new_base_name
+        with self.fs.open(new_path, "w") as f:
+            f.write(orjson.dumps(data).decode("utf-8"))
 
     def extract(self, payload_path: str | Path) -> list[BundleEntry]:
-        bundle = Bundle.parse_file(payload_path)
+        with self.fs.open(payload_path, "rb") as f:
+            data = orjson.loads(f.read())
+        self.save_raw_file(data, Path(payload_path))
+        bundle = Bundle.parse_obj(data)
         if bundle.type != "transaction":
             raise ValueError(f"Expected transaction bundle, got {bundle.type}")
         if bundle.entry is None:
