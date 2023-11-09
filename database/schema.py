d14dcc969ef0c04dbeffe62a173f6a1c13fb93d9
Lyndon Fan <lyndonfan@wessexinsights.com>
2023-11-09 15:33:15 +0000

    wip: just push progress, may not need them

---
+++ database/schema.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, Date, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = "patient"
+
+    id = Column(String, primary_key=True)
+    active = Column(Boolean)
+    gender = Column(String)
+    birthDate = Column(Date)
+    deceasedBoolean = Column(Boolean)
+    deceasedDateTime = Column(DateTime)
+    name = Column(String)
