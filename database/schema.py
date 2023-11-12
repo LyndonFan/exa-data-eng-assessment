9e3e810d4ce59d8183711b7a9c993edd12a0cbbb
Lyndon Fan <lyndon0808@gmail.com>
2023-11-12 11:52:14 +0000

    feat: create patient table

--- database/schema.py
+++ database/schema.py
@@ -1,16 +1,27 @@
-from sqlalchemy import Column, String, Boolean, Date, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Boolean, Date, DateTime
 
-Base = declarative_base()
-
+class Base(DeclarativeBase):
+    pass
 
 class Patient(Base):
     __tablename__ = "patient"
 
-    id = Column(String, primary_key=True)
-    active = Column(Boolean)
-    gender = Column(String)
-    birthDate = Column(Date)
-    deceasedBoolean = Column(Boolean)
-    deceasedDateTime = Column(DateTime)
-    name = Column(String)
+    id: Mapped[str] = mapped_column(primary_key=True)
+    active: Mapped[bool] = mapped_column(nullable=False)
+    name: Mapped[str]
+    maiden_name: Mapped[str]
+    gender: Mapped[str]
+    birth_date = mapped_column(Date)
+    deceased: Mapped[bool]
+    decease_datetime = mapped_column(DateTime)
+    martial_status: Mapped[str]
+
+
+if __name__ == "__main__":
+    from src.db.postgresql import PostgreSQL
+    from sqlalchemy.engine import create_engine
+
+    db = PostgreSQL()
+    engine = create_engine("cockroachdb://", creator=db.connection, echo=True)
+    Base.metadata.create_all(engine)
