29a6bfc297e32e5e162682e90e7b944bfd75af59
Lyndon Fan <lyndon0808@gmail.com>
2023-11-14 01:03:46 +0000

    fix: typos, cast decimal to float

--- src/process/observation_processor.py
+++ src/process/observation_processor.py
@@ -1,3 +1,4 @@
+import decimal
 import polars as pl
 from fhir.resources.R4B.observation import Observation
 
@@ -16,22 +17,22 @@ class ObservationProcessor(BaseProcessor):
     def process(self, data: list[Observation]):
         super().process(data)
         self.save_to_sql(data)
+    
+    def _nested_replace_decimal(self, d):
+        # convert from decimal to float as polars can't decode it
+        # note this does lose some precision which may be problematic
+        if isinstance(d, dict):
+            for k, v in d.items():
+                if isinstance(v, decimal.Decimal):
+                    d[k] = float(v)
+                else:
+                    self._nested_replace_decimal(v)
+        elif isinstance(d, list):
+            for i in d:
+                self._nested_replace_decimal(i)
 
     def process_data_into_frame(self, data: list[Observation]) -> pl.DataFrame:
         dcts = [e.dict() for e in data]
-        """
-
-    id: Mapped[str] = mapped_column(primary_key=True)
-    code: Mapped[str]
-    patient: Mapped[Patient] = relationship(back_populates="observations")
-    patient_id: Mapped[str]
-    encounter: Mapped[Encounter] = relationship(back_populates="observations")
-    encounter_id: Mapped[str]
-    category: Mapped[Optional[str]]
-    values: Mapped[Optional[str]]
-    effective_datetime = mapped_column(DateTime, nullable=True)
-    issued = mapped_column(DateTime, nullable=True)
-        """
         FIELDS = [
             "id",
             "code",
@@ -45,22 +46,21 @@ class ObservationProcessor(BaseProcessor):
             "component",
         ]
         dcts = [{f: d.get(f) for f in FIELDS} for d in dcts]
-        for d in dcts:
-            # convert from decimal to float as polars can't decode it
-            # note this does lose some precision which may be problematic
-            if d["valueQuantity"]:
-                d["valueQuantity"]["value"] = float(d["valueQuantity"]["value"])
-                d["component"] = [d.pop("valueQuantity")]
-            if d["valueCodeableConcept"]:
-                d["component"] = [d.pop("valueCodeableConcept")]
+        self._nested_replace_decimal(dcts)
         df = pl.DataFrame(dcts)
         df = df.with_columns(
             [
-                pl.col("code").coding.list.get(0).display.alias("observation_type"),
+                pl.col("code")
+                .struct.field('coding')
+                .list.get(0)
+                .struct.field('display')
+                .alias("observation_type"),
                 pl.col("category")
                 .list.get(0)
-                .coding.list.get(0)
-                .display.alias("category"),
+                .struct.field('coding')
+                .list.get(0)
+                .struct.field('display')
+                .alias("category"),
                 (
                     pl.col("subject")
                     .struct.field("reference")
@@ -83,4 +83,4 @@ class ObservationProcessor(BaseProcessor):
 
     def save_to_sql(self, data: list[Observation]) -> None:
         df = self.process_data_into_frame(data)
-        self.sql_db.copy_into_table(table_name="observation", df=df)
+        self.sql_db.copy_into_table(table_name="observation", df=df, json_columns=["values"])
