ea618c93e6568433e8473de52998defd83684f35
Lyndon Fan <lyndon0808@gmail.com>
2023-11-15 01:34:44 +0000

    test: main pipeline creates files and writes to tables

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
 import pytest
+
+import os
+import shutil
+from datetime import datetime
 from typing import Optional
 import polars as pl
 from pathlib import Path
@@ -28,23 +32,25 @@ def mock_mongo(mocker):
             return self.collections[collection_name]
 
     class MockMongo:
-        databases = {}
         def __init__(self):
+            self.databases = {}
             pass
-
+        
         def get_database(self, database_name="test"):
             if database_name not in self.databases:
                 self.databases[database_name] = MockDatabase()
             return self.databases[database_name]
-    
-    mocker.patch("src.db.mongo.Mongo", return_value=MockMongo())
+    mongo = MockMongo()
+    mocker.patch("src.db.mongo.Mongo", return_value=mongo)
+    mocker.patch("src.process.base_processor.Mongo", return_value=mongo)
+    return mongo
 
 @pytest.fixture
 def mock_sql(mocker):
     class MockPSQL:
-        values = {}
         def __init__(self):
             self._connection = None
+            self.tables = {}
         def connect(self):
             if self._connection is None:
                 self._connection = mocker.Mock()
@@ -63,15 +69,58 @@ def mock_sql(mocker):
             df: pl.DataFrame,
             json_columns: Optional[list[str]] = None,
         ):
-            if table_name in self.values:
-                self.values[table_name] = pl.concat([
-                    self.values[table_name], df
+            if table_name in self.tables:
+                self.tables[table_name] = pl.concat([
+                    self.tables[table_name], df
                 ])
             else:
-                self.values[table_name] = df
+                self.tables[table_name] = df
         
-    mocker.patch("src.db.postgresql.PostgreSQL", return_value=MockPSQL())
-    return MockPSQL
+    psql = MockPSQL()
+    mocker.patch("src.db.postgresql.PostgreSQL", return_value=psql)
+    mocker.patch("src.process.patient_processor.PostgreSQL", return_value=psql)
+    mocker.patch("src.process.encounter_processor.PostgreSQL", return_value=psql)
+    mocker.patch("src.process.observation_processor.PostgreSQL", return_value=psql)
 
-def test_main(mock_mongo, mock_sql):
+    return psql
+
+@pytest.fixture
+def mock_folder(mocker):
+    mocker.patch.dict(os.environ, {
+        "BUCKET": "test_bucket",
+    })
+    folder = Path("test_bucket")
+    shutil.rmtree(folder, ignore_errors=True)
+    folder.mkdir(parents=True, exist_ok=True)
+    yield folder
+    shutil.rmtree(folder)
+
+def test_main(mock_folder, mock_mongo, mock_sql):
     main(Path("tests/fixtures/bundle_transaction.json"))
+
+    today_date = datetime.today().strftime("%Y-%m-%d")
+    inner_folder = mock_folder / f"upload_date={today_date}"
+    assert inner_folder.is_dir() and inner_folder.exists()
+    expected_pattern = f"bundle_transaction_{today_date}T*.json"
+    found_files = list(mock_folder.rglob(expected_pattern))
+    assert len(found_files) == 1
+
+    assert set(mock_mongo.databases.keys()) == set(["test"])
+    mongo_test_db = mock_mongo.databases["test"]
+    assert set(mongo_test_db.collections.keys()) == set([
+        "Patient", "Encounter", "Observation",
+        "Condition", "Provenance", "IDReference"
+    ])
+    assert len(mongo_test_db.collections["Patient"].values) == 1
+    assert len(mongo_test_db.collections["Encounter"].values) == 1
+    assert len(mongo_test_db.collections["Observation"].values) == 1
+    assert len(mongo_test_db.collections["Condition"].values) == 1
+    assert len(mongo_test_db.collections["Provenance"].values) == 1
+    assert len(mongo_test_db.collections["IDReference"].values) == 5
+
+    assert set(mock_sql.tables.keys()) == set([
+        "patient", "encounter", "observation",
+    ])
+    assert len(mock_sql.tables["patient"]) == 1
+    assert len(mock_sql.tables["encounter"]) == 1
+    assert len(mock_sql.tables["observation"]) == 1
